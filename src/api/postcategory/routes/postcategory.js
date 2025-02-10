ff7a61ad799231447c16bda68f587060af34be92
progbasma <prog.basma@gmail.com>
2025-02-10 17:50:29 +0200

    k

---
+++ src/api/postcategory/routes/postcategory.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * postcategory router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::postcategory.postcategory');
---
+++ src/api/postcategory/services/postcategory.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * postcategory service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::postcategory.postcategory');
